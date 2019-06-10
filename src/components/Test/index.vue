16a368a26491b48e880846b0fd4726f54d3144fa
wenzhang <wenzhang.wang@baichanghui.com>
2019-06-10 20:34:03 +0800

    :leaves:asyncdata automatically merge the returned object with the component data

--- src/components/Test/index.vue
+++ src/components/Test/index.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
-        测试页面
+        {{ test }}
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return {
+            test: 'old',
+        }
+    },
     async asyncData({ store, route }) {
         await new Promise(async (resolve) => {
             await setTimeout(() => {
@@ -12,7 +17,18 @@ export default {
                 resolve()
             }, 2000)
         })
+        return {
+            test: 'async 测试'
+        }
     },
+    mounted(){
+        this.handleTest()
+    },
+    methods: {
+        handleTest() {
+            console.log('--------test mounted')
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
--- src/entry-client.js
+++ src/entry-client.js
@@ -1,22 +1,50 @@
 import Vue from 'vue'
 import { createApp } from './app'
-
-// 客户端特定引导逻辑……
-
 const { app, router, store } = createApp()
+import {
+  getLocation,
+  flatMapComponents,
+  applyAsyncData,
+  setContext,
+  promisify,
+  sanitizeComponent
+} from '../utils'
+
+
+function applySSRData(Component, ssrData) {
+  if (window.__INITIAL_STATE__ && ssrData) {
+    applyAsyncData(Component, ssrData)
+  }
+  Component._Ctor = Component
+  return Component
+}
+
+// Get matched components
+function resolveComponents(router) {
+  const path = getLocation(router.options.base, router.options.mode)
+
+  return flatMapComponents(router.match(path), async (Component, _, match, key, index) => {
+    // If component is not resolved yet, resolve it
+    if (typeof Component === 'function' && !Component.options) {
+      Component = await Component()
+    }
+    // Sanitize it and save it
+    const _Component = applySSRData(sanitizeComponent(Component), window.__INITIAL_STATE__.data ? window.__INITIAL_STATE__.data[index] : null)
+    match.components[key] = _Component
+    return _Component
+  })
+}
+
 
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
 
-router.onReady(() => {
-  // 这里假定 App.vue 模板中根元素具有 id="app"
-  app.$mount('#app')
-
+router.onReady(async () => {
   // Add router hook for handling asyncData.
   // Doing it after initial route is resolved so that we don't double-fetch the data that we already have. // Using `router.beforeResolve()` so that all async components are resolved.
-
-  router.beforeResolve((to, from , next) => {
+  const Components = await Promise.all(resolveComponents(router))
+  router.beforeResolve(async (to, from, next) => {
     const matched = router.getMatchedComponents(to)
     const prevMatched = router.getMatchedComponents(from)
 
@@ -31,15 +59,31 @@ router.onReady(() => {
       return next()
     }
 
+    await setContext(app, {
+      route: router.currentRoute,
+      isHMR: true,
+      next: next.bind(this)
+    })
+    const context = app.context
+    const promises = []
     // this is where we should trigger a loading indicator if there is one
-    Promise.all(activated.map(c => {
-      if (c.asyncData) {
-        // alert('fetch data in client side')
-        return c.asyncData({ store, route: to })
+    Promise.all(activated.map(Component => {
+      if (Component.asyncData) {
+        Component = sanitizeComponent(Component)
+        let promise = promisify(Component.options.asyncData, {
+          store,
+          route: router.currentRoute,
+          cookies: context.cookies,
+        }).then((asyncDataResult) => {
+          applyAsyncData(Component, asyncDataResult)
+        })
+        promises.push(promise)
+        return Promise.all(promises)
       }
     })).then(() => {
       // stop loading indicator
       next()
     }).catch(next)
   })
+  app.$mount('#app')
 })
